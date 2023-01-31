f6f8613de162ec919c0176d9680f85497d30e487
Alin <alin@gmail.com>
2023-01-31 13:44:28 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-
+import { useGetAllProductsQuery, useGetSingleProductQuery } from "./store/productsApi";
+import { useState, useEffect } from "react";
+import { ClipLoader } from "react-spinners";
 
 function App() {
+
+
+  const { data: allData } = useGetAllProductsQuery();
+
+
+  const [input, setInput] = useState("");
+  const { data: singleData, error, isLoading, isFetching, isError } = useGetSingleProductQuery(input);
+
+  useEffect(() => {
+    if (singleData) {
+      console.log(singleData.products);
+    }
+  }, [singleData])
+
+
   return (
     <div className="App">
 
-    </div>
+      {(isLoading || isFetching) && <ClipLoader color="#000000" className="loader" size="100" />}
+
+      <div className="search">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+
+      <div className="data">
+        {singleData && singleData.products.map(single => <div key={single.id} className="product">
+          <h1>{single.title}</h1>
+          <img src={single.images[1]} />
+        </div>)}
+      </div>
+
+
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "./index.css";
+
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { productsApi } from './store/productsApi';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={productsApi}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 ); 
